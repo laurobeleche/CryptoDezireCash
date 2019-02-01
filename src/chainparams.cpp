93e50a23285fd7309aacb33f9ae7d4d21af87770
CryptoDezire <43305087+cryptodezire@users.noreply.github.com>
2019-02-01 22:05:13 +0100

    added checkpoint

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -65,11 +65,12 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
     (   50102, uint256("0x852e3327005be853df57d7bb8e91b5528276c987031e4ddb59c50a66d7619f6b"))
     (  148990, uint256("0x737811aa1da3d7273a70b687ace80ee4486562ab14c84a626c9210a063e3d363"))
     (  156170, uint256("0xaf3dd7ba23486f057bd8daccf93223f6c685865d29d2b40b813f2fe798727f95"))
+    (  161645, uint256("0xbbc89be95110a0580bad26cb35b24bcac8e53d37cedaec48381be82e9041c9cb"))
     ; 
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
-    1548721653, // * UNIX timestamp of last checkpoint block
-    316668,      // * total number of transactions between genesis and last checkpoint
+    1549054948, // * UNIX timestamp of last checkpoint block
+    327931,      // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
     5000        // * estimated number of transactions per day after checkpoint
 };
